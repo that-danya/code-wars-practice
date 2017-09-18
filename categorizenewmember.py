de07d1b2ed500f10868433efd74fb5ca79533503
Danya Dye <danya.dye@gmail.com>
2017-09-18 13:58:56 -0700

    Adjusted logic, all tests passed on categorize problem.

--- categorizenewmember.py
+++ categorizenewmember.py
@@ -2,7 +2,7 @@ def openOrSenior(data):
     placement = []
 
     for age, handic in data:
-        if age >= 55 and handic >= 7:
+        if age >= 55 and handic > 7:
             placement.append('Senior')
         else:
             placement.append('Open')
